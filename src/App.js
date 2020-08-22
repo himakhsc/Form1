9e1b40991f04d39edf6c9520128e613412b4538c
himakhsc <hima.khsc@gmail.com>
2020-08-22 18:23:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      fullName:''
+    }
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const data = this.state
+    console.log("Finla data is", data);
+  }
+
+  handleInputChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  componentDidMount(){
+    this.setState({
+      fullName: "puli"
+    })
+  }
+
+  render(){
+      const {fullName} = this.state
+    return(
+      <div>
+        <p>Full Name is: {fullName}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p><input type='text' placeholder='your Name' name='fullName' value={fullName} onChange={this.handleInputChange}/></p>
+          <p><button>Send Messge</button></p>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App;  
